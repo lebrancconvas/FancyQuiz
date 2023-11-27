b34648ef9becd505fc28c91810581fe7c147063b
lebrancconvas <lebrancconvas@gmail.com>
2023-11-27 08:44:09 +0700

    ✨ feat: draft report api

--- backend/controllers/report.go
+++ backend/controllers/report.go
@@ -1,3 +1,17 @@
 package controllers
 
+import "github.com/gin-gonic/gin"
+
 type ReportController struct{}
+
+func (r ReportController) GetAllReport(c *gin.Context) {
+
+}
+
+func (r ReportController) GetReportFromDateCreated(c *gin.Context) {
+	
+}
+
+func (r ReportController) CreateReport(c *gin.Context) {
+
+}
