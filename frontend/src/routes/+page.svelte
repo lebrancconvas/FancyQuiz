d65d25868729885303585948666ae0b5f3af3310
lebrancconvas <lebrancconvas@gmail.com>
2023-11-27 14:05:33 +0700

    ✨ feat: update landing page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+
+</script>
+
+<svelte:head>
+  <title>Fancy Quiz</title>
+</svelte:head>
+
+<main>
+  <header>
+    <h1>Fancy Quiz</h1>
+  </header>
+  <footer>
+    <p>Copyright 2023, Made with Love by <a href="https://github.com/lebrancconvas" target="_blank">Poom Yimyuean (@lebrancconvas)</a> | <a href="https://github.com/lebrancconvas/FancyQuiz" target="_blank">View Github Repo</a></p>
+  </footer>
+</main>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  header {
+    text-align: center;
+  }
+  footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 50px;
+    text-align: center;
+    background-color: white;
+    font-size: 21px;
+  }
+</style>
