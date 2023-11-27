b13424db8766cf47669c4fbe9b4eaca89452411d
lebrancconvas <lebrancconvas@gmail.com>
2023-11-27 08:45:56 +0700

    ✨ feat: draft history api

--- backend/controllers/history.go
+++ backend/controllers/history.go
@@ -1,3 +1,17 @@
 package controllers
 
+import "github.com/gin-gonic/gin"
+
 type HistoryController struct{}
+
+func (h HistoryController) GetAllHistory(c *gin.Context) {
+
+}
+
+func (h HistoryController) GetHistoryFromUser(c *gin.Context) {
+
+}
+
+func (h HistoryController) CreateHistory(c *gin.Context) {
+	
+}
