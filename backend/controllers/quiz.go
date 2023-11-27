a8ced37d841240b939db6643b25d3693c212e123
lebrancconvas <lebrancconvas@gmail.com>
2023-11-28 06:58:16 +0700

    ✨ feat: (wip) update create quiz controller

--- backend/controllers/quiz.go
+++ backend/controllers/quiz.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lebrancconvas/FancyQuiz/models"
@@ -36,7 +37,30 @@ func (q QuizController) GetAllQuizCategory(c *gin.Context) {
 }
 
 func (q QuizController) CreateQuiz(c *gin.Context) {
+	var userID uint64 = 0
+	categoryID := c.PostForm("category_id")
+	title := c.PostForm("title")
+	description := c.PostForm("description")
 
+	md := new(models.Quiz)
+
+	categoryIDInt, err := strconv.Atoi(categoryID)
+	if err != nil {
+		utils.UnprocessableLog(c, err)
+		return
+	}
+
+	_, err = md.CreateQuiz(userID, uint64(categoryIDInt), title, description)
+	if err != nil {
+		utils.UnprocessableLog(c, err)
+		return
+	}
+
+	// WIP
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Create Quiz Success!",
+	})
 }
 
 func (q QuizController) UpdateQuiz(c *gin.Context) {
