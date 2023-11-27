ec476f674d44c42f5e20b7bc29fd7c1ea3d0bba8
lebrancconvas <lebrancconvas@gmail.com>
2023-11-27 07:41:56 +0700

    ✨ feat: (wip) draft all quiz controller function

--- backend/controllers/quiz.go
+++ backend/controllers/quiz.go
@@ -1,3 +1,29 @@
 package controllers
 
+import "github.com/gin-gonic/gin"
+
 type QuizController struct{}
+
+func (q QuizController) GetAllQuiz(c *gin.Context) {
+
+}
+
+func (q QuizController) GetAllQuizFromCreatedUser(c *gin.Context) {
+
+}
+
+func (q QuizController) GetAllQuizFromParticipatedUser(c *gin.Context) {
+
+}
+
+func (q QuizController) CreateQuiz(c *gin.Context) {
+
+}
+
+func (q QuizController) UpdateQuiz(c *gin.Context) {
+
+}
+
+func (q QuizController) DeleteQuiz(c *gin.Context) {
+	
+}
