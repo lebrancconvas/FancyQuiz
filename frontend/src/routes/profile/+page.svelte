ee5a5caa6cc8b1cf5d5d1b9740f7d71c00ed7f21
lebrancconvas <lebrancconvas@gmail.com>
2023-11-30 00:05:39 +0700

    ✨ feat: create user profile page

---
+++ frontend/src/routes/profile/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  
+</script>
+
+<svelte:head>
+  <title>Fancy Quiz | Profile</title>
+</svelte:head>
+
+<main>
+  <header>
+    <h1>Profile</h1>
+  </header>
+</main>
+
+<style>
+
+</style>
